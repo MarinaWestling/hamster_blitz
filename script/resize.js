316bece6f4cf2fb99d9ee00be8a86d5882d61396
MarinaWestling <marinamwestling@gmail.com>
2020-09-28 14:18:16 -0400

    integration

--- script/resize.js
+++ script/resize.js
@@ -1,7 +1,5 @@
 // From https://www.html5rocks.com/en/tutorials/casestudies/gopherwoord-studios-resizing-html5-games/#disqus_thread
 
-
-
 function resizeGame() {
   let gameArea = document.querySelector('#gameArea');
   let widthToHeight = 16 / 9;
@@ -28,11 +26,7 @@ function resizeGame() {
   gameArea.style.marginTop = (-newHeight / 2) + 'px';
   gameArea.style.marginLeft = (-newWidth / 2) + 'px';
   // Adjust Font Size
-  gameArea.style.fontSize = (newWidth / 400) + 'em';
-
-  let gameCanvas = document.querySelector('#gameCanvas');
-  gameCanvas.width = newWidth;
-  gameCanvas.height = newHeight;
+  gameArea.style.fontSize = (newWidth / 400) + 'em';    
 }
 
 resizeGame();
--- script/start_screen.js
+++ script/start_screen.js
@@ -7,15 +7,6 @@ function replayGame(){
   modal.style.display = 'block';
 }
 
-// function ready() {
-//   const { type } = screen.orientation;
-// }
-
-// async function start() {
-//   await document.body.requestFullscreen();
-//   await screen.orientation.lock("landscape");
-//   ready();
-// }
 function adjustheight(){
   const th = document.documentElement.clientHeight;
   document.getElementById('game').style.height = `${th}px`;
@@ -29,9 +20,6 @@ function adjustHeight(){
   var split = lh/2;
   var adjust = `${split}px`;
   document.getElementById('startbtn').style.marginTop = adjust;
-  console.log("Game box height is "+th);
-  console.log("Button height is " + gh);
-  console.log("Difference is "+lh);
-  console.log(split);
-  console.log(adjust);
 }
+
+adjustHeight();
