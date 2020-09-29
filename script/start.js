0e02385fbc8f5c32779164292fd2d39a2dd77445
Marina Westling <marinamwestling@gmail.com>
2020-09-28 22:25:27 -0400

    fixed replay menu

--- script/start.js
+++ script/start.js
@@ -12,13 +12,23 @@ function hideReplayMenu() {
 }
 
 function adjustHeight(){
+ 
   const th = document.getElementById('start').offsetHeight;
   const gh = document.getElementById('startbtn').offsetHeight;
   const titleHeight = document.getElementById('title').offsetHeight;
+  const statHeight = document.getElementById('stats').offsetHeight;
+  
+  //adjust start button height
   var lh = th-gh;
   var split = (lh/2)-titleHeight;
   var adjust = `${split}px`;
   document.getElementById('startbtn').style.marginTop = adjust;
+  console.log(th, gh, lh, titleHeight, split);
+
+  //adjust replay height
+  var divide = lh/2-statHeight;
+  var addText = `${divide}px`;
+  document.getElementById('replaybtn').style.marginTop = addText;
 }
 
 adjustHeight();
